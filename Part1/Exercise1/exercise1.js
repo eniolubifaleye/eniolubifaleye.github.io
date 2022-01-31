50f47ab7c857a3a23773f047a21e850d5d6037c1
eniolubifaleye <eniolubifaleye@gmail.com>
2022-01-31 14:24:34 +0000

    Add exercise1.js

---
+++ Part1/Exercise1/exercise1.js
@@ -0,0 +1,10 @@
+//used select to select first paragraph class and change font family to sans serif
+d3.select("p").style("color", "red");
+d3.select("p").style("font-family", "sans-serif");
+    
+//used select all to change all the paragraph classes font size to 30px
+d3.selectAll("p").style("font-size", "30px");
+ 
+// write your d3 code here..  
+console.log('d3.version:', d3.version); 
+console.log('version 7.3 is shown on the console'); 
