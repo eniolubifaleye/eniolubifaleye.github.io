432e23e9d2522e61d158823fdfb6897ccbea4d9d
eniolubifaleye <eniolubifaleye@gmail.com>
2022-02-03 13:53:58 +0000

    updated index html and index js for link to different html exercises

--- index.js
+++ index.js
@@ -9,3 +9,39 @@ document.getElementById("link2").onmousedown = function(event) {
         window.open("./Part2/Exercise3 and Exercise4/exercise3 and exercise4.html")
     }
 }
+
+document.getElementById("link3").onmousedown = function(event) {
+    if (event.which == 3) {
+        window.open("./Part2/Exercise5/exercise5.html")
+    }
+}
+
+document.getElementById("link4").onmousedown = function(event) {
+    if (event.which == 3) {
+        window.open("./Part3/Exercise6/exercise6.html")
+    }
+}
+
+document.getElementById("link5").onmousedown = function(event) {
+    if (event.which == 3) {
+        window.open("./Part3/Exercise7/exercise7.html")
+    }
+}
+
+document.getElementById("link6").onmousedown = function(event) {
+    if (event.which == 3) {
+        window.open("./Part4/Exercise8/exercise8.html")
+    }
+}
+
+document.getElementById("link7").onmousedown = function(event) {
+    if (event.which == 3) {
+        window.open("./Part5/Exercise9/exercise9.html")
+    }
+}
+
+document.getElementById("link8").onmousedown = function(event) {
+    if (event.which == 3) {
+        window.open("./Part5/Exercise10/exercise10.html")
+    }
+}
