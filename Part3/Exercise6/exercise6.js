1aeca7688804fc7a021331981f1bbc3e1f434a2c
eniolubifaleye <eniolubifaleye@gmail.com>
2022-02-03 14:51:29 +0000

    Update exercise6.js

--- Part3/Exercise6/exercise6.js
+++ Part3/Exercise6/exercise6.js
@@ -1,21 +1,26 @@
 let nums = [10, 50, 200]; 
 
-let otherdata = [ {name:'test',  val:1},  
-                  {name:'other', val:2}, 
-                  {name:'b',     val:3},
+//in otherdata array of objects, add new variable color and set each eleent to red green and blue
+let otherdata = [ {name:'test',  val:1, color: 'red'},  
+                  {name:'other', val:2, color: 'green'}, 
+                  {name:'b',     val:3, color: 'blue'},
                   
-                  //in other array of objects, add new object with name color and val 4
-                  {name:'color', val:4}];
+         ];
                   
 let paragraph = d3.select("body") 
                 .selectAll("div") 
                 .data(otherdata) 
                 .text(function (d, i) { 
                     console.log("d.name: " + d.name); 
-                    console.log("d.val:  " + d.val); 
+                    console.log("d.val:  " + d.val);
+                    console.log("d.color: " + d.color)
                     console.log("i: " + i); 
                     console.log("this: " + this); 
 										
-                    // return value is used to set the 'text' and the name
-                    return 'cont:' + d.name + " , " + "val:" +  d.val;  
-                }); 
+                   // return value is used to set the 'text' and the name, the value and the color
+                    return 'cont: ' + d.name + " , " + "val: " +  d.val + " , " + "color: " + d.color;  
+                })
+                
+                //set the color of the text of each object "each line displayed" to be the color set
+                //in the object
+                .style("color", function(d,i) {return d.color}); 
