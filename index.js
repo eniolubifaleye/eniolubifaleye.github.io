702fc8ca71664a33b0ee4cc089298c1ad5a4d027
eniolubifaleye <eniolubifaleye@gmail.com>
2022-02-03 12:45:56 +0000

    Updated index page to have id for the right click load html and added index.js file for loading html page with right click

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById("link1").onmousedown = function(event) {
+    if (event.which == 3) {
+        window.open("./Part2/Exercise1 and Exercise2/exercise1 and exercise2.html")
+    }
+}
+
+document.getElementById("link2").onmousedown = function(event) {
+    if (event.which == 3) {
+        window.open("./Part2/Exercise1 and Exercise2/exercise3 and exercise4.html")
+    }
+}
+
+console.log("a")
